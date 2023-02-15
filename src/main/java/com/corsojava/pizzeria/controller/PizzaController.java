8f282256e1ecf32c08b2054a1dea8e7537155238
Giovanni Ciotta <giociotta@gmail.com>
2023-02-15 17:57:02 +0100

    ricerca e link click

--- src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
+++ src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
@@ -1,28 +1,51 @@
 package com.corsojava.pizzeria.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.corsojava.pizzeria.model.Pizza;
 import com.corsojava.pizzeria.repository.PizzaRepository;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/pizze")
 public class PizzaController {
 
 	@Autowired
 	PizzaRepository pizzaRepository;
 
 	@GetMapping
-	public String index(Model model) {
-		List<Pizza> elencoPizze = pizzaRepository.findAll();
-		model.addAttribute("elencoPizze", elencoPizze);
-		return "Pizze/index";
+	public String index(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
+		List<Pizza> elencoPizze;
 
+		if (keyword == null) {
+			elencoPizze = pizzaRepository.findAll();
+		} else {
+			elencoPizze = pizzaRepository.findByNomeLike("%" + keyword + "%");
+			;
+		}
+
+		model.addAttribute("elencoPizze", elencoPizze);
+		return "pizze/index";
+
+	}
+
+	@GetMapping("/{id}")
+	public String detail(@PathVariable("id") Integer id, Model model) {
+		Optional<Pizza> p = pizzaRepository.findById(id);
+		if (p.isPresent()) {
+			model.addAttribute("pizza", p.get());
+		} else {
+			return "pizze/index";
+		}
+
+		return "pizze/detail";
 	}
 }
--- src/main/java/com/corsojava/pizzeria/repository/PizzaRepository.java
+++ src/main/java/com/corsojava/pizzeria/repository/PizzaRepository.java
@@ -1,9 +1,12 @@
 package com.corsojava.pizzeria.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.corsojava.pizzeria.model.Pizza;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
 
+	public List<Pizza> findByNomeLike(String keyword);
 }
