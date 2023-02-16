83384adbc3842e1028599a63d2ebc0d1f8996dc6
Giovanni Ciotta <giociotta@gmail.com>
2023-02-16 17:36:35 +0100

    create and validation

--- src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
+++ src/main/java/com/corsojava/pizzeria/controller/PizzaController.java
@@ -6,22 +6,32 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.corsojava.pizzeria.model.Pizza;
 import com.corsojava.pizzeria.repository.PizzaRepository;
 
+import jakarta.validation.Valid;
+
 @Controller
-@RequestMapping("/pizze")
+@RequestMapping("/")
 public class PizzaController {
+
+	@GetMapping
+	public String home() {
+		return "pizze/home";
+	}
 
 	@Autowired
 	PizzaRepository pizzaRepository;
 
-	@GetMapping
+	@GetMapping("/pizze")
 	public String index(@RequestParam(name = "keyword", required = false) String keyword, Model model) {
 		List<Pizza> elencoPizze;
 
@@ -47,5 +57,25 @@ public class PizzaController {
 		}
 
 		return "pizze/detail";
+	}
+
+	@GetMapping("/create")
+
+	public String create(Model model) {
+		Pizza pizza = new Pizza();
+
+		model.addAttribute("pizza", pizza);
+		return "pizze/create";
+	}
+
+	@PostMapping("/create")
+	public String store(@Valid @ModelAttribute("pizza") Pizza formPizza, BindingResult bindingResult, Model model) {
+
+		if (bindingResult.hasErrors())
+			return "pizze/create";
+
+		pizzaRepository.save(formPizza);
+		return "redirect:/pizze";
+
 	}
 }
--- src/main/java/com/corsojava/pizzeria/model/Pizza.java
+++ src/main/java/com/corsojava/pizzeria/model/Pizza.java
@@ -1,5 +1,12 @@
 package com.corsojava.pizzeria.model;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,10 +21,20 @@ public class Pizza {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
+	@NotNull
+	@NotEmpty(message = "Questo è un campo obbligatorio")
+	@Size(min = 2, max = 20, message = "il nome  deve contere dai {min} ai {max} caratteri")
 	private String nome;
+
+	@NotNull
+	@NotEmpty(message = "Questo è un campo obbligatorio")
 	private String descrizione;
+
 	private String foto;
-	private float prezzo;
+	@NotNull(message = "Questo è un campo obbligatorio")
+//	@NotEmpty(message = "Questo è un campo obbligatorio")
+	@DecimalMin(value = "1.00", message = "il prezzo deve essere maggiore di zero")
+	private BigDecimal prezzo;
 
 	public String getNome() {
 		return nome;
@@ -43,11 +60,11 @@ public class Pizza {
 		this.foto = foto;
 	}
 
-	public float getPrezzo() {
+	public BigDecimal getPrezzo() {
 		return prezzo;
 	}
 
-	public void setPrezzo(float prezzo) {
+	public void setPrezzo(BigDecimal prezzo) {
 		this.prezzo = prezzo;
 	}
 
